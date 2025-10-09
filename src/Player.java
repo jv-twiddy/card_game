2095c4694db2cd2056e580f1905384355af3ef5a
andrew-tembra <130045328+andrew-tembra@users.noreply.github.com>
2025-10-09 15:18:50 +0100

    added run method to Player class

--- src/Player.java
+++ src/Player.java
@@ -2,4 +2,24 @@ package src;
 
 public class Player extends Thread{
     
+    private volatile boolean done = false;
+
+    @Override
+    public void run (){
+        while (!done) {
+            try {
+                Thread.sleep(10); 
+                 System.out.println("AAAAAAAAAA");
+            } catch (InterruptedException e) {
+                System.out.println("Thread interrupted");
+                break;
+            }
+           
+        }
+    }
+
+    public void stopThread() {
+        System.out.println("Stopping thread");
+        this.done = true;
+    }
 }
